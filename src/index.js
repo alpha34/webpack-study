790d82ba03a1d82faeb741325f7a4dea53e3003c
hk-mac <hk34.kim@kt.com>
2024-08-01 23:06:00 +0900

    Feat. webpack study

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import getRandomAddress from "../src/randomAddress";
+import { nanoid } from "nanoid";
+import "./styles/main.scss";
+
+console.log(nanoid());
+console.log(nanoid());
+console.log(getRandomAddress());
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  //시작점
+  entry: {
+    main: path.resolve(__dirname, "src/index.js"),
+  },
+  // 웹팩 작업을 통해 생성된 겨로가물
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+  },
+};
